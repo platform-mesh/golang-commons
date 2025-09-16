d4c4e0de95a8799a89eb018d23550e725dce91ba
Bastian Echterhölter <bastian.echterhoelter@sap.com>
2025-09-16 18:46:30 +0200

    fix(model): improve error message for id_token parsing
    • Avoid printing PII information

--- jwt/model.go
+++ jwt/model.go
@@ -37,7 +37,7 @@ type WebToken struct {
 func New(idToken string, signatureAlgorithms []jose.SignatureAlgorithm) (webToken WebToken, err error) {
 	token, parseErr := jwt.ParseSigned(idToken, signatureAlgorithms)
 	if parseErr != nil {
-		err = fmt.Errorf("unable to parse id_token: [%s], %w", idToken, parseErr)
+		err = fmt.Errorf("unable to parse id_token: %w", parseErr)
 		return
 	}
 
